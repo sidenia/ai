b2293c8cb42207df6f661e307f88ffab094be373
sidenia <sideniabezerra@hotmail.com>
2025-09-01 15:43:15 -0300

    update files

--- news/app/trash.py
+++
@@ -1,9 +0,0 @@
-#  print(f'✅Notícias do Brasil geradas com sucesso!')
-#     with open("resumos_brasil.txt", "w", encoding="utf-8") as f:
-#         for resumo in resumos_brasil:
-#             f.write(resumo + "\n")
-
-#     print(f'\n✅Notícias do Mundo geradas com sucesso!')
-#     with open("resumos_mundo.txt", "w", encoding="utf-8") as f:
-#         for resumo in resumos_mundo:
-#             f.write(resumo + "\n")
